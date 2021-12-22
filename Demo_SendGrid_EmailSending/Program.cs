56ba278bb87b87a5f39abb48f1485691b1c6887f
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2021-12-22 13:52:25 -0500

    Initial commit

---
+++ Demo_SendGrid_EmailSending/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_SendGrid_EmailSending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
